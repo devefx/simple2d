24b9e994507c7e506293f7ee2535496e1d1e2f40
devefx <devefx@163.com>
2016-06-15 16:54:08 +0800

    define safe release macros

--- libsimple2d/simple/platform/PlatformMacros.h
+++ libsimple2d/simple/platform/PlatformMacros.h
@@ -23,4 +23,16 @@
 
 
 
+
+#define SAFE_DELETE(p)           do { delete (p); (p) = nullptr; } while(0)
+#define SAFE_DELETE_ARRAY(p)     do { if(p) { delete[] (p); (p) = nullptr; } } while(0)
+#define SAFE_FREE(p)             do { if(p) { free(p); (p) = nullptr; } } while(0)
+#define SAFE_RELEASE(p)          do { if(p) { (p)->release(); } } while(0)
+#define SAFE_RELEASE_NULL(p)     do { if(p) { (p)->release(); (p) = nullptr; } } while(0)
+#define SAFE_RETAIN(p)           do { if(p) { (p)->retain(); } } while(0)
+#define BREAK_IF(cond)           if(cond) break
+
+
+
+
 #endif // !__PLATFORM_MACROS_H__
